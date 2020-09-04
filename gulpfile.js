c0c60069c274a1d0a1dada68693c779a95f7b6c7
Gevork <64646814+socialcolor@users.noreply.github.com>
2020-09-05 02:38:21 +0300

    Валидация html

--- gulpfile.js
+++ gulpfile.js
@@ -135,6 +135,7 @@ exports.server = server;
 
 const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
+  gulp.watch("source/*.html", gulp.series("html"));
   gulp.watch("source/*.html").on("change", sync.reload);
   gulp.watch("source/js/*.js", gulp.series("js"));
 }
--- source/js/common.js
+++ source/js/common.js
@@ -1,28 +1,28 @@
-var nav = document.querySelector('.nav__list');
-var navToggle = document.querySelector('.nav__toggle')
-var navImg = document.querySelector('.nav__toggle img')
-var productLink = document.querySelectorAll('.product__button');
-var productLinkImg = document.querySelectorAll('.product__link-images');
+var nav = document.querySelector(".nav__list");
+var navToggle = document.querySelector(".nav__toggle")
+var navImg = document.querySelector(".nav__toggle img")
+var productLink = document.querySelectorAll(".product__button");
+var productLinkImg = document.querySelectorAll(".product__link-images");
 
 
-navToggle.addEventListener('click', function () {
-  nav.classList.toggle('nav__list--active');
-  if (nav.classList.contains('nav__list--active')) {
-    navToggle.classList.add('nav__icon-close');
+navToggle.addEventListener("click", function () {
+  nav.classList.toggle("nav__list--active");
+  if (nav.classList.contains("nav__list--active")) {
+    navToggle.classList.add("nav__icon-close");
   } else {
-    navToggle.classList.remove('nav__icon-close');
-    navToggle.classList.add('nav__icon-open');
+    navToggle.classList.remove("nav__icon-close");
+    navToggle.classList.add("nav__icon-open");
   }
 });
 
-document.addEventListener('DOMContentLoaded', function(e) {
-  nav.classList.remove('nav__list--no-js');
-  navToggle.classList.add('nav__icon-open');
+document.addEventListener("DOMContentLoaded", function(e) {
+  nav.classList.remove("nav__list--no-js");
+  navToggle.classList.add("nav__icon-open");
 
   for(var i = 0; i < productLink.length; i++) {
-    productLink[i].removeAttribute('href')
-    productLink[i].setAttribute('href', '#')
-    productLinkImg[i].removeAttribute('href')
-    productLinkImg[i].setAttribute('href', '#')
+    productLink[i].removeAttribute("href")
+    productLink[i].setAttribute("href", "#")
+    productLinkImg[i].removeAttribute("href")
+    productLinkImg[i].setAttribute("href", "#")
   }
 });
